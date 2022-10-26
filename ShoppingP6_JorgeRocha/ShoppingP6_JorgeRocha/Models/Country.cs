21c7b95bd48a329528122f90500383272bceb049
JorgeRochaA <jor2019.cr@gmail.com>
2022-10-26 17:08:57 -0600

    Semana 7

---
+++ ShoppingP6_JorgeRocha/ShoppingP6_JorgeRocha/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingP6_JorgeRocha.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            //Stores = new HashSet<Store>();
+            Users = new HashSet<User>();
+        }
+
+        public int Idcountry { get; set; }
+        public int CountryCode { get; set; }
+        public string CountryName { get; set; } = null!;
+        public bool? Active { get; set; }
+
+        //public virtual ICollection<Store> Stores { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ ShoppingP6_JorgeRocha/ShoppingP6_JorgeRocha/Models/UserRole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingP6_JorgeRocha.Models
+{
+    public class UserRole
+    {
+        public UserRole()
+        {
+            Users = new HashSet<User>();
+        }
+
+        public int IduserRole { get; set; }
+        public string UserRoleDescription { get; set; } = null!;
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
